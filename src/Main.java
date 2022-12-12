9c802b07cc8ab6760a83e5b9c5fddd13ce7eb862
Константин <kuz18rus@ya.ru>
2022-12-12 17:01:53 +0400

    задания 1-3 выполнены

---
+++ src/Main.java
@@ -0,0 +1,67 @@
+public class Main {
+    public static void main(String[] args) {
+        task1();
+        task2();
+        task3();
+    }
+
+    public static void task1() {
+        System.out.println("Задача 1");
+        int[] weights = new int[3];
+        weights[0] = 1;
+        int one = weights[0];
+        weights[1] = 2;
+        int two = weights[1];
+        weights[2] = 3;
+        int three = weights[2];
+        double[] weights1 = {1.57, 7.654, 9.986};
+        int[] weights2 = {12, 8, 55, 14, 9};
+    }
+
+    public static void task2() {
+        System.out.println("Задача 2");
+        int[] weights = new int[3];
+        weights[0] = 1;
+        int one = weights[0];
+        weights[1] = 2;
+        int two = weights[1];
+        weights[2] = 3;
+        int three = weights[2];
+        double[] weights1 = {1.57, 7.654, 9.986};
+        int[] weights2 = {12, 8, 55, 14};
+        System.out.print(weights[0] + ", " + weights[1] + ", " + weights[2] + ", ");
+        System.out.println();
+        System.out.print(weights1[0] + ", " + weights1[1] + ", " + weights1[2]);
+        System.out.println();
+        System.out.print(weights2[0] + ", " + weights2[1] + ", " + weights2[2] + ", " + weights2[3]);
+        System.out.println();
+    }
+    public static void task3() {
+        System.out.println("Задача 3");
+        int[] weights = new int[3];
+        weights[0] = 1;
+        int one = weights[0];
+        weights[1] = 2;
+        int two = weights[1];
+        weights[2] = 3;
+        int three = weights[2];
+        double[] weights1 = {1.57, 7.654, 9.986};
+        int[] weights2 = {12, 8, 55, 14};
+        System.out.print(weights[2] + ", " + weights[1] + ", " + weights[0] + ", ");
+        System.out.println();
+        System.out.print(weights1[2] + ", " + weights1[1] + ", " + weights1[0]);
+        System.out.println();
+        System.out.print(weights2[3] + ", " + weights2[2] + ", " + weights2[1] + ", " + weights2[0]);
+        System.out.println();
+    }
+    public static void task4() {
+        System.out.println("Задача 4");
+        int[] weights = new int[3];
+        weights[0] = 1;
+        int one = weights[0];
+        weights[1] = 2;
+        int two = weights[1];
+        weights[2] = 3;
+        int three = weights[2];
+    }
+}
